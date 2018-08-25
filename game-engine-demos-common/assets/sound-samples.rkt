bcc624e94d65a97e3ae46586bfbc4ce0b4c408f6
aBlender <jasonlongle@gmail.com>
2018-08-25 12:22:03 -0700

    Added Dialog Sounds

--- game-engine-demos-common/assets/sound-samples.rkt
+++ game-engine-demos-common/assets/sound-samples.rkt
@@ -4,7 +4,12 @@
          CAT-SOUND
          DOG-SOUND
          HEY-SOUND
-         ZOOP-SOUND)
+         ZOOP-SOUND
+         PICKUP-SOUND
+         BLIP-SOUND
+         SHORT-BLIP-SOUND
+         OPEN-DIALOG-SOUND
+         CLOSE-DIALOG-SOUND)
 
 (require rsound
          racket/runtime-path)
@@ -15,9 +20,21 @@
 (define-runtime-path hey-path "hey.wav")
 (define-runtime-path zoop-path "zoop.wav")
 
+(define-runtime-path pickup-path "pickup.wav")
+(define-runtime-path blip-path "blip.wav")
+(define-runtime-path short-blip-path "short_blip.wav")
+(define-runtime-path open-dialog-path "open_dialog.wav")
+(define-runtime-path close-dialog-path "close_dialog.wav")
+
 (define DUCK-SOUND (resample-to-rate 48000 (rs-read duck-path)))
 (define CAT-SOUND  (resample-to-rate 48000 (rs-read cat-path)))
 (define DOG-SOUND  (resample-to-rate 48000 (rs-read dog-path)))
 (define HEY-SOUND  (resample-to-rate 48000 (rs-read hey-path)))
 (define ZOOP-SOUND (resample-to-rate 48000 (rs-read zoop-path)))
+
+(define PICKUP-SOUND       (resample-to-rate 48000 (rs-read pickup-path)))
+(define BLIP-SOUND         (resample-to-rate 48000 (rs-read blip-path)))
+(define SHORT-BLIP-SOUND   (resample-to-rate 48000 (rs-read short-blip-path)))
+(define OPEN-DIALOG-SOUND  (resample-to-rate 48000 (rs-read open-dialog-path)))
+(define CLOSE-DIALOG-SOUND (resample-to-rate 48000 (rs-read close-dialog-path)))
 
