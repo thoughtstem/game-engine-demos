ed011ba623068ee80a3370276d90431419040b83
aBlender <jasonlongle@gmail.com>
2018-11-09 18:21:31 -0800

    Fancier game over screen

--- game-engine-demos-common/game-over-screen.rkt
+++ game-engine-demos-common/game-over-screen.rkt
@@ -5,11 +5,12 @@
 (require game-engine)
 
 (define (end-screen w h msg color)
-  (new-sprite (list (overlay (text msg 30 color)
+  (new-sprite (list (overlay #;(text msg 30 color)
+                             (tint-img color (draw-title msg))
                              (rectangle w h "solid" (make-color 0 0 0 100))))
               1))
 
-(define lose-screen (lambda (w h) (end-screen w h "GAME OVER!" "yellow")))
+(define lose-screen (lambda (w h) (end-screen w h "GAME OVER!" "red")))
 (define win-screen  (lambda (w h) (end-screen w h "YOU WIN!" "green")))
 
 (define (game-over-screen won? lost?)
